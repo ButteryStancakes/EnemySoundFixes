c83a03281821b183ebcecd3280dd16e73dbbd433
ButteryStancakes <jlj3322@gmail.com>
2024-05-13 13:49:17 -0400

    Giant update

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# v1.1.0
+- Polish pass on forest keepers
+- Reworked some snare flea code (to fix a broken patch that I replaced last minute before release)
+# v1.0.0
+- Initial release
--- Plugin.cs
+++ Plugin.cs
@@ -2,7 +2,6 @@
 using BepInEx.Logging;
 using HarmonyLib;
 using System.Collections.Generic;
-//using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Reflection.Emit;
@@ -13,7 +12,7 @@ namespace EnemySoundFixes
     [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-        const string PLUGIN_GUID = "butterystancakes.lethalcompany.enemysoundfixes", PLUGIN_NAME = "Enemy Sound Fixes", PLUGIN_VERSION = "1.0.0";
+        const string PLUGIN_GUID = "butterystancakes.lethalcompany.enemysoundfixes", PLUGIN_NAME = "Enemy Sound Fixes", PLUGIN_VERSION = "1.1.0";
         internal static new ManualLogSource Logger;
 
         void Awake()
@@ -29,7 +28,8 @@ namespace EnemySoundFixes
     [HarmonyPatch]
     class EnemySoundFixesPatches
     {
-        static AudioClip /*hitEnemyBody,*/ baboonTakeDamage/*, centipedeWalk*/;
+        static AudioClip /*hitEnemyBody,*/ baboonTakeDamage;
+        static readonly FieldInfo CREATURE_VOICE = typeof(EnemyAI).GetField(nameof(EnemyAI.creatureVoice), BindingFlags.Instance | BindingFlags.Public);
 
         [HarmonyPatch(typeof(QuickMenuManager), "Start")]
         [HarmonyPostfix]
@@ -54,34 +54,23 @@ namespace EnemySoundFixes
                 }
                 baboon.enemyType.hitBodySFX = null; //hitEnemyBody
                 Plugin.Logger.LogInfo("Overwritten baboon hawk damage sound");
+                baboon.enemyType.enemyPrefab.GetComponent<BaboonBirdAI>().dieSFX = baboon.enemyType.deathSFX;
+                Plugin.Logger.LogInfo("Overwritten missing baboon hawk death sound");
             }
-            /*if (centipedeWalk == null)
+            SpawnableEnemyWithRarity centipede = __instance.testAllEnemiesLevel.Enemies.FirstOrDefault(spawnableEnemyWithRarity => spawnableEnemyWithRarity.enemyType.name == "Centipede");
+            if (centipede != null)
             {
-                try
-                {
-                    AssetBundle assetBundle = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "enemysoundfixes"));
-                    centipedeWalk = assetBundle.LoadAsset<AudioClip>("CentipedeWalk");
-                    assetBundle.Unload(false);
-                }
-                catch
-                {
-                    Plugin.Logger.LogError("Failed to load assets from asset bundle (\"enemysoundfixes\" file); did you install the plugin correctly?");
-                }
+                centipede.enemyType.enemyPrefab.GetComponent<CentipedeAI>().creatureSFX.loop = true;
+                Plugin.Logger.LogInfo("Loop snare flea walking and clinging");
             }
-            if (centipedeWalk != null)
+            SpawnableEnemyWithRarity giant = __instance.testAllEnemiesLevel.OutsideEnemies.FirstOrDefault(spawnableEnemyWithRarity => spawnableEnemyWithRarity.enemyType.name == "ForestGiant");
+            if (giant != null)
             {
-                SpawnableEnemyWithRarity centipede = __instance.testAllEnemiesLevel.Enemies.FirstOrDefault(spawnableEnemyWithRarity => spawnableEnemyWithRarity.enemyType.name == "Centipede");
-                if (centipede != null)
-                {
-                    EnemyBehaviourState attacking = centipede.enemyType.enemyPrefab.GetComponent<CentipedeAI>().enemyBehaviourStates.FirstOrDefault(behaviorState => behaviorState.name == "attacking");
-                    if (attacking != null)
-                    {
-                        attacking.SFXClip = centipedeWalk;
-                        Plugin.Logger.LogInfo("Overwritten snare flea chase sound effect (10s)");
-                    }
-                }
+                giant.enemyType.enemyPrefab.GetComponent<ForestGiantAI>().creatureSFX.spatialBlend = 1f;
+                Plugin.Logger.LogInfo("Fix forest giant global audio volume");
+                giant.enemyType.hitBodySFX = StartOfRound.Instance.footstepSurfaces.FirstOrDefault(footstepSurface => footstepSurface.surfaceTag == "Wood").hitSurfaceSFX;
+                Plugin.Logger.LogInfo("Overwritten missing forest giant hit sound");
             }
-            */
         }
 
         [HarmonyPatch(typeof(NutcrackerEnemyAI), nameof(NutcrackerEnemyAI.KillEnemy))]
@@ -116,42 +105,27 @@ namespace EnemySoundFixes
             }
         }
 
-        [HarmonyPatch(typeof(BaboonBirdAI), nameof(BaboonBirdAI.KillEnemy))]
-        [HarmonyPrefix]
-        static void BaboonBirdAIPreKillEnemy(BaboonBirdAI __instance)
-        {
-            if (__instance.dieSFX == null)
-                __instance.dieSFX = __instance.enemyType.deathSFX;
-            Plugin.Logger.LogInfo($"Baboon hawk: Overwritten missing death sound");
-        }
-
         [HarmonyPatch(typeof(CentipedeAI), "delayedShriek", MethodType.Enumerator)]
         [HarmonyTranspiler()]
-        static IEnumerable<CodeInstruction> transDelayedShriek(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        static IEnumerable<CodeInstruction> CentipedeAITransDelayedShriek(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
         {
             List<CodeInstruction> codes = instructions.ToList();
             Label label = generator.DefineLabel();
 
-            int insertAt = -1;
             for (int i = 0; i < codes.Count - 1; i++)
             {
                 //Plugin.Logger.LogInfo(codes[i]);
-                if (codes[i].opcode == OpCodes.Ldloc_1 && codes[i + 1].opcode == OpCodes.Ldfld && (FieldInfo)codes[i + 1].operand == typeof(EnemyAI).GetField(nameof(EnemyAI.creatureVoice), BindingFlags.Instance | BindingFlags.Public))
+                if (codes[i].opcode == OpCodes.Ldloc_1 && codes[i + 1].opcode == OpCodes.Ldfld && (FieldInfo)codes[i + 1].operand == CREATURE_VOICE)
                 {
-                    insertAt = i;
                     codes[i].labels.Add(label);
+                    codes.Insert(i, new CodeInstruction(OpCodes.Ldloc_1, null));
+                    codes.Insert(i + 1, new CodeInstruction(OpCodes.Ldfld, typeof(EnemyAI).GetField(nameof(EnemyAI.isEnemyDead), BindingFlags.Instance | BindingFlags.Public)));
+                    codes.Insert(i + 2, new CodeInstruction(OpCodes.Brfalse, label));
+                    codes.Insert(i + 3, new CodeInstruction(OpCodes.Ldc_I4_0, null));
+                    codes.Insert(i + 4, new CodeInstruction(OpCodes.Ret, null));
+                    Plugin.Logger.LogInfo("Transpiler: Patched Centipede shriek (added isEnemyDead check)");
                     break;
                 }
-            }
-
-            if (insertAt != -1)
-            {
-                codes.Insert(insertAt, new CodeInstruction(OpCodes.Ldloc_1, null));
-                codes.Insert(insertAt + 1, new CodeInstruction(OpCodes.Ldfld, typeof(EnemyAI).GetField(nameof(EnemyAI.isEnemyDead), BindingFlags.Instance | BindingFlags.Public)));
-                codes.Insert(insertAt + 2, new CodeInstruction(OpCodes.Brfalse, label));
-                codes.Insert(insertAt + 3, new CodeInstruction(OpCodes.Ldc_I4_0, null));
-                codes.Insert(insertAt + 4, new CodeInstruction(OpCodes.Ret, null));
-                Plugin.Logger.LogInfo("Transpiler: Patched Centipede shriek (added isEnemyDead check)");
             }
 
             return codes;
@@ -161,34 +135,10 @@ namespace EnemySoundFixes
         [HarmonyPrefix]
         static void CentipedeAIPreUpdate(CentipedeAI __instance)
         {
-            if (__instance.creatureSFX.isPlaying)
+            if (__instance.creatureSFX.isPlaying && __instance.creatureSFX.clip.name == "CentipedeWalk" && (__instance.isEnemyDead || __instance.currentBehaviourState.name != "attacking"))
             {
-                if (__instance.creatureSFX.clip.name == "monsterNoise2")
-                {
-                    if (__instance.creatureSFX.loop)
-                    {
-                        __instance.creatureSFX.loop = false;
-                        Plugin.Logger.LogInfo($"Snare flea: Don't loop ceiling grapple noises");
-                    }
-                    if (__instance.creatureVoice.pitch > 1f)
-                    {
-                        __instance.creatureVoice.pitch = 1f;
-                        Plugin.Logger.LogInfo($"Snare flea: Reset \"voice\" pitch for attacking again");
-                    }
-                }
-                else
-                {
-                    if (__instance.creatureSFX.clip.name == "CentipedeWalk" && (__instance.isEnemyDead || __instance.currentBehaviourState.name != "attacking"))
-                    {
-                        __instance.creatureSFX.Stop();
-                        Plugin.Logger.LogInfo($"Snare flea: Stop walking while dead, clinging to player, or sneaking away");
-                    }
-                    else if (!__instance.creatureSFX.loop && !__instance.isEnemyDead)
-                    {
-                        __instance.creatureSFX.loop = true;
-                        Plugin.Logger.LogInfo($"Snare flea: Loop walking and clinging");
-                    }
-                }
+                __instance.creatureSFX.Stop();
+                Plugin.Logger.LogInfo("Snare flea: Stop walking while dead, clinging to player, or sneaking away");
             }
         }
 
@@ -196,8 +146,90 @@ namespace EnemySoundFixes
         [HarmonyPostfix]
         static void CentipedeAIPostKillEnemy(CentipedeAI __instance)
         {
-            // randomize death pitch
             __instance.creatureVoice.pitch = Random.value > 0.5f ? 1f : 1.7f;
+            Plugin.Logger.LogInfo("Snare flea: Randomize death screech pitch");
+        }
+
+        [HarmonyPatch(typeof(EnemyAI), nameof(EnemyAI.PlayAudioOfCurrentState))]
+        [HarmonyPostfix]
+        static void EnemyAIPostPlayAudioOfCurrentState(EnemyAI __instance)
+        {
+            if (__instance is CentipedeAI && __instance.currentBehaviourState.name == "hiding" && __instance.creatureVoice.pitch > 1f)
+            {
+                __instance.creatureVoice.pitch = 1f;
+                Plugin.Logger.LogInfo("Snare flea: Reset \"voice\" pitch for attacking again");
+            }
+        }
+
+        [HarmonyPatch(typeof(ForestGiantAI), nameof(ForestGiantAI.Update))]
+        [HarmonyPostfix]
+        static void ForestGiantAIPostUpdate(ForestGiantAI __instance)
+        {
+            if (__instance.stunNormalizedTimer > 0f || __instance.isEnemyDead || __instance.currentBehaviourState.name == "Burning")
+            {
+                PlayAudioAnimationEvent playAudioAnimationEvent = __instance.animationContainer.GetComponent<PlayAudioAnimationEvent>();
+                if (playAudioAnimationEvent != null)
+                {
+                    AudioSource closeWideSFX = playAudioAnimationEvent.audioToPlay;
+                    if (closeWideSFX.isPlaying)
+                    {
+                        closeWideSFX.Stop();
+                        Plugin.Logger.LogInfo("Forest keeper: Stop chewing non-existent player");
+                    }
+                    ParticleSystem bloodParticle = playAudioAnimationEvent.particle;
+                    if (bloodParticle.isEmitting)
+                    {
+                        bloodParticle.Stop();
+                        Plugin.Logger.LogInfo("Forest keeper: Don't spray blood from mouth");
+                    }
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(ForestGiantAI), "StopKillAnimation")]
+        [HarmonyPatch(typeof(ForestGiantAI), "EatPlayerAnimation", MethodType.Enumerator)]
+        [HarmonyTranspiler()]
+        static IEnumerable<CodeInstruction> ForestGiantAITransAnimation(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> codes = instructions.ToList();
+
+            for (int i = 1; i < codes.Count - 1; i++)
+            {
+                //Plugin.Logger.LogInfo(codes[i]);
+                if (codes[i].opcode == OpCodes.Ldfld && (FieldInfo)codes[i].operand == CREATURE_VOICE)
+                {
+                    for (int j = i - 1; j <= i + 1; j++)
+                    {
+                        codes[j].opcode = OpCodes.Nop;
+                        codes[j].operand = null;
+                    }
+                    Plugin.Logger.LogInfo("Transpiler: Don't interrupt forest keeper voice");
+                    break;
+                }
+            }
+
+            return codes;
+        }
+
+        [HarmonyPatch(typeof(ForestGiantAI), nameof(ForestGiantAI.AnimationEventA))]
+        [HarmonyPostfix]
+        static void ForestGiantAIPostAnimationEventA(ForestGiantAI __instance)
+        {
+            __instance.creatureSFX.PlayOneShot(__instance.giantFall);
+            Plugin.Logger.LogInfo("Forest keeper: Fallen down");
+        }
+
+        [HarmonyPatch(typeof(PlayAudioAnimationEvent), nameof(PlayAudioAnimationEvent.PlayAudio2))]
+        [HarmonyPrefix]
+        static bool PlayAudioAnimationEventPrePlayAudio2(PlayAudioAnimationEvent __instance)
+        {
+            if (__instance.audioClip2.name == "FGiantEatPlayerSFX")
+            {
+                __instance.audioToPlay.PlayOneShot(__instance.audioClip2);
+                Plugin.Logger.LogInfo("Forest keeper: Play bite sound effect with overlap");
+                return false;
+            }
+            return true;
         }
     }
 }
