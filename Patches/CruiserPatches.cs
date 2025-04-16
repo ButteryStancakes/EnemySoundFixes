1651dec2909d129239b3627d36a979dfc2a9b932
ButteryStancakes <jlj3322@gmail.com>
2025-04-15 21:05:22 -0400

    #6 Fix typo when checking Cruiser wheels

--- Patches/CruiserPatches.cs
+++ Patches/CruiserPatches.cs
@@ -67,7 +67,7 @@ namespace EnemySoundFixes.Patches
         [HarmonyPrefix]
         static void VehicleControllerPreSetVehicleAudioProperties(VehicleController __instance, AudioSource audio, ref bool audioActive)
         {
-            if (audioActive && ((audio == __instance.extremeStressAudio && __instance.magnetedToShip) || ((audio == __instance.rollingAudio || audio == __instance.skiddingAudio) && (__instance.magnetedToShip || (!__instance.FrontLeftWheel.isGrounded && !__instance.FrontRightWheel.isGrounded && !__instance.BackLeftWheel.isGrounded && !__instance.FrontLeftWheel.isGrounded)))))
+            if (audioActive && ((audio == __instance.extremeStressAudio && __instance.magnetedToShip) || ((audio == __instance.rollingAudio || audio == __instance.skiddingAudio) && (__instance.magnetedToShip || (!__instance.FrontLeftWheel.isGrounded && !__instance.FrontRightWheel.isGrounded && !__instance.BackLeftWheel.isGrounded && !__instance.BackRightWheel.isGrounded)))))
                 audioActive = false;
         }
 
